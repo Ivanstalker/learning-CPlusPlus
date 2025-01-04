b89dab882d861227de0cbcee8f9ea155dca8e867
Ivanstalker <160216845+Ivanstalker@users.noreply.github.com>
2025-01-04 15:19:21 +0200

    learning cpp 1

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 
 int main() {
     system("chcp 65001");
-    // запрашивает имя
-    std::cout << "введите своё имя: ";
-    std::string name;
+    // запрашивает имя для программы
+    std::cout << "придумай мне имя: " << std::endl;
+    std::string  name;
     std::cin >> name;
-    // запрашивает возраст
-    std::cout << "введите свой возраст: ";
+    // напиши сколько мне лет
+    std::cout << "напиши сколько мне лет: " << std::endl;
     std::string years;
     std::cin >> years;
-    // запрашивает город проживания
-    std::cout << "введите город проживания: ";
+    // где программа живет
+    std::cout << "напиши где я живу: " << std::endl;
     std::string home;
-    std::cin >> home;
+    std ::cin >> home;
+    // напиши чем я увлекаюсь
+    std::cout << "напиши чем я увлекаюсь: " << std::endl;
+    std::string hobby;
+    std ::cin >> hobby;
     // обобщение
-    std::cout << "Здравствуйте, меня зовут " << name  << " мне " << years << " лет "<< std::endl;
-    std::cout << "Я живу в городе " << home << std::endl;
-
-
+    std::cout << "здравствуйте, меня зовут " << name << ", мне " << years << " лет\n Я живу в " << home << ", я увлекаюсь " << hobby << std::endl;
     return 0;
 }
